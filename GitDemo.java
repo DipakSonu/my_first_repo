69da05b01bf870fd71ec47654adac4eca98224db
Dipak <dipakkumarsonu@outlook.com>
2023-01-01 15:54:37 +0530

    Second Change

--- GitDemo.java
+++ GitDemo.java
@@ -7,5 +7,15 @@ public class GitDemo {
         System.out.println("Dipak is far far away from you. Keep working utill you reach to your The Goal");
 
         System.out.println("Hello Next change");
+
+        int a=10;
+
+        int b=50;
+
+        int result=0;
+
+        result = a*b+(a+b);
+
+        System.out.println("\n\n Result "+result);
     }
 }
