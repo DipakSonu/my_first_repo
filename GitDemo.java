983e211aa77010083d47a7520888c37c5b5af077
Dipak <dipakkumarsonu@outlook.com>
2023-01-08 13:42:13 +0530

    8-1-2023 Revision

--- GitDemo.java
+++ GitDemo.java
@@ -2,8 +2,6 @@ public class GitDemo {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello Git");
-
         System.out.println("Dipak is far far away from you. Keep working utill you reach to your The Goal");
 
         System.out.println("Hello Next change");
